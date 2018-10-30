9841a7ceff11679f6852775d8a67fad3f1845df4
zhanyang <zhanyang_work@163.com>
2018-10-31 00:59:29 +0800

    update

---
+++ src/main/java/com/used/beimei/used_eureka_server/EurekaServer.java
@@ -0,0 +1,12 @@
+package com.used.beimei.used_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer.class, args);
+	}
+}
