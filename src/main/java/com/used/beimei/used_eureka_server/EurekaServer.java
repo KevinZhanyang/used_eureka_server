6d8ec1438794d769f4af8feed4d65208b5525ed7
zhanyang <zhanyang_work@163.com>
2018-10-31 01:07:22 +0800

    update

--- src/main/java/com/used/beimei/used_eureka_server/EurekaServer.java
+++ src/main/java/com/used/beimei/used_eureka_server/EurekaServer.java
@@ -2,8 +2,10 @@ package com.used.beimei.used_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServer {
 
 	public static void main(String[] args) {
